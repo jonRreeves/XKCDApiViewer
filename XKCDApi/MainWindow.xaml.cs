a54c8115e108c3b7731c63425ab901978b712b56
Jon <jonreeves1995@gmail.com>
2019-04-04 11:51:59 +0100

    Uploading Project

---
+++ XKCDApi/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using APILibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XKCDApi
+
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private int maxNumber = 0;
+        private int currentNumber = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+            next.IsEnabled = false;
+        }
+
+        private async Task LoadImage(int imageNumber = 0)
+        {
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+
+            if (imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+            }
+
+            currentNumber = comic.Num;
+
+            var uriSource = new Uri(comic.Img, UriKind.Absolute);
+            comicImage.Source = new BitmapImage(uriSource);
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadImage();
+        }
+
+        private async void Previous_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber > 1)
+            {
+                currentNumber -= 1;
+                next.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if (currentNumber == 1)
+                {
+                    previous.IsEnabled = false;
+                }
+            }
+        }
+
+        private async void Next_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber < maxNumber)
+            {
+                currentNumber += 1;
+                previous.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if (currentNumber == maxNumber)
+                {
+                    next.IsEnabled = false;
+                }
+
+            }
+        }
+    }
+}
